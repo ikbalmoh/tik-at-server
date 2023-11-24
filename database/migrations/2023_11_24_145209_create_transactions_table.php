c1736d506f6cd69a599caa015ff4f9286b59932f
Ikbal <hikmat.iqbal@gmail.com>
2023-11-24 22:09:47 +0700

    model: Transaction

---
+++ database/migrations/2023_11_24_145209_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->unsignedBigInteger('operator_id');
+            $table->smallInteger('qty')->default(1);
+            $table->double('price');
+            $table->double('subtotal');
+            $table->double('discount')->default(0);
+            $table->double('total');
+            $table->double('pay');
+            $table->double('charge')->default(0);
+            $table->string('payment_method')->default('cash');
+            $table->timestamps();
+
+            $table->foreign('type_id')->references('id')->on('ticket_types');
+            $table->foreign('operator_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
