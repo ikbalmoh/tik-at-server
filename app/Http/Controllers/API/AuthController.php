e214b3056205d17c6d61c1ba0ef85cafe38d557e
Ikbal <hikmat.iqbal@gmail.com>
2023-11-25 10:51:22 +0700

    route: always return json for api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -12,32 +12,25 @@ class AuthController extends Controller
 {
     public function authOperator(Request $request)
     {
-        try {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required',
-                'device_name' => 'required',
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'password' => ['Periksa kembali PIN anda'],
             ]);
-
-            $user = User::where('email', $request->email)->first();
-
-            if (!$user || !Hash::check($request->password, $user->password)) {
-                throw ValidationException::withMessages([
-                    'password' => ['Periksa kembali PIN anda'],
-                ]);
-            }
-
-            $user->tokens()->delete();
-            $token = $user->createToken($request->device_name, ['ticket:transaction'])->plainTextToken;
-            return response()->json([
-                'token' => $token,
-                'message' => 'Authenticated'
-            ]);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'token' => null,
-                'message' => $th->getMessage()
-            ], 422);
         }
+
+        $user->tokens()->delete();
+        $token = $user->createToken($request->device_name, ['ticket:transaction'])->plainTextToken;
+        return response()->json([
+            'token' => $token,
+            'message' => 'Authenticated'
+        ]);
     }
 }
