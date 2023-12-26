dccb47ef5034ecce67bf61c80865b930edbad6d2
Ikbal <hikmat.iqbal@gmail.com>
2023-12-26 19:39:37 +0700

    ticket type: build

---
+++ public/build/assets/ApplicationLogo-7b3b6ea8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-8928a867.js";function e(i){return t.jsx("div",{...i,children:"eTiket Bagendit"})}export{e as A};
---
+++ public/build/assets/cls-ac679db6.js
@@ -0,0 +1,1 @@
+function c(...n){return n.join(" ")}export{c};
