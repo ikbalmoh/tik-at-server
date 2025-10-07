c1bf8f5c2e4348a098ddcfc9a8784b78c1115fd2
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-10-07 22:59:51 +0700

    build

---
+++ public/build/assets/ApplicationLogo-79527cb6.js
@@ -0,0 +1,1 @@
+import{q as r,j as i}from"./app-56708db8.js";function n(o){const{appName:p}=r().props;return i.jsx("div",{...o,children:p??"GarTix"})}export{n as A};
--- public/build/assets/ApplicationLogo-e546ed53.js
+++
@@ -1,1 +0,0 @@
-import{q as r,j as i}from"./app-d84d6bfd.js";function n(o){const{appName:p}=r().props;return i.jsx("div",{...o,children:p??"GarTix"})}export{n as A};
