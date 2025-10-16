244bc14ab5a5ba2af235e393ca7811a43edf07e0
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-10-16 23:12:52 +0700

    build

---
+++ public/build/assets/ApplicationLogo-703ea67c.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-265ca0db.js";function o(i){return t.jsx("div",{...i,children:"eTiket Pariwisata Garut"})}export{o as A};
--- public/build/assets/ApplicationLogo-79527cb6.js
+++
@@ -1,1 +0,0 @@
-import{q as r,j as i}from"./app-56708db8.js";function n(o){const{appName:p}=r().props;return i.jsx("div",{...o,children:p??"GarTix"})}export{n as A};
